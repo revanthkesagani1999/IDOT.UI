d6032ca0ac3bad79634bc87200d542b13ab41702
Revanth Kesagani <k.revanthgoud903@gmail.com>
2024-07-01 10:00:53 -0500

    password reset

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
+import { NotificationService } from '../_services/notification.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -12,7 +13,9 @@ export class ResetPasswordComponent {
   
   constructor(
     private activatedRoute: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private router: Router
   ) {
     this.token = this.activatedRoute.snapshot.params['token'];
   }
@@ -20,17 +23,22 @@ export class ResetPasswordComponent {
   resetPassword(newPassword: string, confirmPassword: string) {
     if (newPassword !== confirmPassword) {
       // Handle password mismatch
-      console.error('Passwords do not match');
+      //console.error('Passwords do not match');
+      this.notificationService.triggerNotification('Passwords do not match', 'error');
       return;
     }
     this.authService.resetPassword(this.token, newPassword).subscribe({
       next: (response) => {
         // Handle success response
         console.log(response);
+        this.notificationService.triggerNotification('Password reset is successful', 'success');
+        this.router.navigate(['/login']);
       },
       error: (error) => {
         // Handle error response
         console.error(error);
+        this.notificationService.triggerNotification('Passwords reset is failed', 'error');
+     
       }
     });
   }
